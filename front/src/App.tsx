0fe9f7a74df5fc398da4d1030bc6391d8affa472
edsonarios <edsonrios9@gmail.com>
2023-11-10 15:47:53 -0400

    Added base reproductor using plyr winth function drang and drop

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import PlyrComponent from './player-video'
 
 function App () {
   return (
     <>
       <div>
-        <h1>Conversor</h1>
+      <h1 className='mt-10'>Video Editor</h1>
+      <PlyrComponent />
       </div>
     </>
   )
---
+++ front/src/player-video.tsx
@@ -0,0 +1,91 @@
+import { useState, useRef } from 'react'
+import Plyr, { type APITypes, type PlyrOptions } from 'plyr-react'
+
+function PlyrComponent () {
+  const [videoSrc, setVideoSrc] = useState('')
+  const playerRef = useRef<APITypes>(null)
+  const [cutStart, setCutStart] = useState<number | null>(null)
+  const [cutEnd, setCutEnd] = useState<number | null>(null)
+
+  const plyrOptions: PlyrOptions = {
+    loop: { active: true },
+    autoplay: true,
+    hideControls: false,
+    keyboard: {
+      global: true
+    }
+  }
+
+  const handleStartCut = () => {
+    if (playerRef?.current !== null) {
+      const player = playerRef.current.plyr
+      setCutStart(player.currentTime)
+    }
+  }
+
+  const handleEndCut = () => {
+    if (playerRef?.current !== null) {
+      const player = playerRef.current.plyr
+      setCutEnd(player.currentTime)
+    }
+  }
+
+  const jumpToSecond = (seconds: number | null) => {
+    if (playerRef?.current !== null && seconds !== null) {
+      const player = playerRef.current.plyr
+      player.currentTime = seconds
+    }
+  }
+
+  console.log(cutStart, cutEnd, videoSrc)
+
+  const handleDragOver = (event: any) => {
+    event.preventDefault()
+  }
+
+  const handleDrop = (event: any) => {
+    event.preventDefault()
+    const files = event.dataTransfer.files
+
+    if ((Boolean(files)) && files.length > 0) {
+      const file = files[0]
+      const url = URL.createObjectURL(file)
+      setVideoSrc(url)
+    }
+  }
+  return (
+    <div
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      style={{ width: '100%', height: '400px', border: '1px dashed #ccc' }}
+    >
+      <Plyr
+        ref={playerRef}
+        source={{
+          type: 'video',
+          sources: [
+            {
+              src: videoSrc,
+              provider: 'html5'
+            }
+          ]
+        }}
+        options={plyrOptions}
+      />
+      <button onClick={() => {
+        if (playerRef?.current !== null) {
+          jumpToSecond(playerRef.current.plyr.currentTime - 1)
+        }
+      }}>Retroceder 1s</button>
+      <button onClick={() => {
+        if (playerRef?.current !== null) {
+          jumpToSecond(playerRef.current.plyr.currentTime + 1)
+        }
+      }}>Avanzar 1s</button>
+      <button onClick={handleStartCut}>Marcar Inicio</button>
+      <button onClick={handleEndCut}>Marcar Fin</button>
+    </div>
+  )
+}
+
+export default PlyrComponent
