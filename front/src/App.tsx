83e63d1398024cf3a3c44a4aaf0ead47c95a9a7e
edsonarios <edsonrios9@gmail.com>
2023-11-10 16:34:09 -0400

    Added tailwind and defined a style basic for reproductor and configuration options

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
-import PlyrComponent from './player-video'
+import PlyrComponent from './components/player-video'
 
 function App () {
   return (
     <>
       <div>
-      <h1 className='mt-10'>Video Editor</h1>
-      <PlyrComponent />
+        <div className="flex justify-center">
+          <h1 className="text-4xl font-bold my-4">Video Conversor</h1>
+        </div>
+
+        <PlyrComponent />
       </div>
     </>
   )
